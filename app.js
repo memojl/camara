321d69e976e254d169fd6ba4ff676ad444f35d69
memojl <memojl08@gmail.com>
2022-03-07 18:49:49 -0600

    Camara

---
+++ app.js
@@ -0,0 +1,9 @@
+navigator.mediaDevices.getUserMedia({
+    audio:false,
+    video:true
+}).then((stream)=>{
+    console.log(stream)
+    let video = document.getElementById('camara');
+    video.srcObject = stream;
+    video.onloadedmetadata = (ev) => video.play()
+}).catch((err)=>{console.log(err)})
