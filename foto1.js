0f2b0351ecb187212589e67c97fcd7c29e79838e
FrontHitss <programador.hitss@gmail.com>
2022-03-09 11:35:43 -0600

    09.03.2022

---
+++ foto1.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const video = document.getElementById('video');
+const snap = document.getElementById("snap");
+const canvas = document.getElementById('canvas');
+const errorMsgElement = document.querySelector('span#errorMsg');
+
+const constraints = {
+audio: false,
+video: {width: 640, height: 480}
+};
+
+// Acceso a la webcam
+async function init() {
+try {
+const stream = await navigator.mediaDevices.getUserMedia(constraints);
+handleSuccess(stream);
+} catch (e) {
+//errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+}
+}
+// Correcto!
+function handleSuccess(stream) {
+window.stream = stream;
+video.srcObject = stream;
+}
+// Load init
+init();
+// Dibuja la imagen
+var context = canvas.getContext('2d');
+snap.addEventListener("click", function() {
+context.drawImage(video, 0, 0, 640, 480);
+});
