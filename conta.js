507ee1018435c91ab934c9ae5857283002cffb6e
memojl <memojl08@gmail.com>
2022-03-26 00:48:20 -0600

    contador final

--- conta.js
+++ conta.js
@@ -1,15 +1,10 @@
 function counter(id, start, end) {
     var i = (start>end)?start+1:start-1;
-    var end = end;
     var display = document.getElementById(id);
     var timer = setInterval(function() {
-        if(i>end){
-            i--;
-        }else{
-            i++;
-        }
+        if(i>end){i--;}else{i++;}
         if(i==end){clearInterval(timer);}
         display.innerHTML = i;
     }, 1000);
 }
-counter('display', 5, 0);
+counter('display', 0, 10);
